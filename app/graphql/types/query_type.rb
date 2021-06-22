aafbcdd0bf7ceabbc15166ad54735930514b92e0
ryunosuke35 <6bev2103@gmail.com>
2021-06-23 01:18:06 +0900

    as

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,4 +10,10 @@ Types::QueryType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  field :blog, !Types::BlogType do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:blog]
+    }
+  end
 end
